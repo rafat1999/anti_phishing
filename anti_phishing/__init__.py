97191e67f082cd180a990491edbbc3f3adbf5f27
Shihab Hossen Rafat <100786024+rafat1999@users.noreply.github.com>
2025-01-08 21:09:02 +0600

    Update __init__.py

--- anti_phishing/__init__.py
+++ anti_phishing/__init__.py
@@ -1,1 +1,6 @@
+from .core import FirebaseAntiPhishing
+from .database import DatabaseManager
+from .utils import print_banner
 
+__version__ = "1.0.0"
+__all__ = ["FirebaseAntiPhishing", "DatabaseManager", "print_banner"]
